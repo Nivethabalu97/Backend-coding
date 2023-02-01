1e78983797ef4eeba1aef2f1373e3abbcf884b56
nivethabalu97 <nivethabalu97@gmail.com>
2023-02-01 23:12:44 +0530

    created blueprint

--- signupapi.py
+++ signupapi.py
@@ -2,112 +2,81 @@ import mysql.connector
 from flask import Flask
 from flask import request
 import datetime
-import re
+import logging
+from log import *
+
+from flask import Blueprint
+from genericfunctions import check_password, check_data_type, check_alphabets, check_mob_num, check_email, check_fields, currentdate
 
 
-
-
-
-app = Flask(__name__)
+# app = Flask(__name__)
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Nivetha@123",
-  database="sampledatabase"
+    host="localhost",
+    user="root",
+    password="Nivetha@123",
+    database="sampledatabase"
 )
 
+signup_blueprint = Blueprint('signup_blueprint', __name__)
 mycursor = mydb.cursor()
-@app.route("/postUsers2", methods=["POST"])
+
+
+@signup_blueprint.route("/signup", methods=["POST"])
 def get_post_Request():
     getData = request.json
-    d=check(getData)
-    d=alpha(getData)
-    d=num(getData)
-    d=check1(getData)
-    id=generateid(getData)
-    getData["id"]=id
-    if d == True:
+    result_datatype = check_data_type(getData)
+    result_alphabets = check_alphabets(getData)
+    result_mobnumber = check_mob_num(getData)
+    result_email = check_email(getData)
+    result_password = check_password(getData)
+    fields = ["firstname", "lastname", "emailid",
+              "mobilenumber", "password", "dob"]
+    result_fields = check_fields(fields, getData)
+    id = generateid(getData)
+    getData["id"] = id
+    if result_datatype["status"] and result_alphabets["status"] and result_mobnumber["status"] and result_email["status"] and result_password["status"] and result_fields["status"] == True:
         try:
+
             signuptime = datetime.datetime.now()
             getData["signuptime"] = signuptime
             sql = "INSERT INTO signup (fname,lname,email,mobilenumber,password,dob,signuptime,id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
-            val = (getData["firstname"],getData["lastname"],getData["emailid"],getData["mobilenumber"],getData["password"],getData["dob"],getData["signuptime"],getData["id"])
+            val = (getData["firstname"], getData["lastname"], getData["emailid"], getData["mobilenumber"],
+                   getData["password"], getData["dob"], getData["signuptime"], getData["id"])
             mycursor.execute(sql, val)
             mydb.commit()
-            filter=getData["sno"]
-            sql1= "SELECT * FROM transactions WHERE sno="+filter
+            logger_log.info(id)
+            filter = getData["password"]
+            sql1 = "SELECT * FROM signup WHERE password='"+filter+"'"
             print(sql1)
             mycursor.execute(sql1)
             myresult = mycursor.fetchall()
             print(myresult)
-            if(len(myresult))!=0:
-                message="Successfully stored and status code: 201 "
+            if (len(myresult)) != 0:
+                message = "Successfully stored and status code: 200 "
                 return message
             else:
-                message="Not Stored"
+                message = "Not Stored"
+                logger.error(message)
                 return message
         except TypeError as error:
-            msg="Sorry, invalid datatype" + str(error)
+            msg = "Sorry, invalid datatype" + str(error)
+            logger.error(msg)
             return msg
         except Exception as error:
-            message1="Sorry, your data cannot be stored in database" +str(error)
+            message1 = "Sorry, your data cannot be stored in database" + \
+                str(error)
+            logger.error(message1)
             return message1
     else:
-        return d,"422"
-def check(a):
-    for i,j in a.items():
-        if type(j)!= str:
-            print("please enter valid data")
-            b="datatype of "+ str(i)+"is invalid"
-            return {"status":False,"message":b}
-        else:
-            print("valid data")
-    c="all the data are valid type"
-    return {"status":True,"message":c}
+        logger.error("something wrong")
+        return ("something went wrong"), "422"
+
+
 def generateid(getData):
-    fname=(getData["firstname"])
-    name=""
-    for i in range(0,4):
-        name=name+fname[i]
-    count="000"
-    name=name+"_"+count
+    fname = (getData["firstname"])
+    name = ""
+    for i in range(0, 4):
+        name = name+fname[i]
+    count = "000"
+    name = name+"_"+count
     return name
-def alpha(getData):
-    fname=(getData["firstname"])
-    lname=(getData["lastname"])
-    x=fname.isalpha()
-    y=lname.isalpha()
-    if x and y == True:
-        z="True"
-        return z
-    else:
-        z="firstname and lastname should not contain any integers"
-        return z
-def num(getData):
-    x=str(getData["mobilenumber"])
-    y=(x.isdigit())
-    if len(x)==10 and y==True:
-        z="True"
-        print(z)
-    else:
-        z="mob number should be of strictly 10 digits and should not contain any alphabets"
-        return z
-def check1(getData):
-    email=getData["emailid"]
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
-    if(re.fullmatch(regex, email)):
-        z="True"
-        return z
-    else:
-        z="Invalid Email"
-        return z
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-   app.run()
