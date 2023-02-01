0c24f7aa66555ec356dc996635df7e3cbe1f9985
nivethabalu97 <nivethabalu97@gmail.com>
2023-02-01 23:10:28 +0530

    single time run server

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from passwordcheck import login_blueprint
+from signupapi import signup_blueprint
+from storetransactionwitherrorhandling import transaction_blueprint
+from sample import sample_blueprint
+from storetransaction import store_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(login_blueprint)
+app.register_blueprint(signup_blueprint)
+app.register_blueprint(transaction_blueprint)
+app.register_blueprint(sample_blueprint)
+app.register_blueprint(store_blueprint)
+
+if __name__ == '__main__':
+    app.run()
