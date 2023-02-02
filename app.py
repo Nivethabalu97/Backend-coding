073cb1d296d9dd4cc5d5088bc16a04503bb9ccc9
nivethabalu97 <nivethabalu97@gmail.com>
2023-02-02 18:35:49 +0530

    latest app.py file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from signupapi import signup_blueprint
 from storetransactionwitherrorhandling import transaction_blueprint
 from sample import sample_blueprint
 from storetransaction import store_blueprint
+from sam import sam_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(login_blueprint)
@@ -11,6 +12,8 @@ app.register_blueprint(signup_blueprint)
 app.register_blueprint(transaction_blueprint)
 app.register_blueprint(sample_blueprint)
 app.register_blueprint(store_blueprint)
+app.register_blueprint(sam_blueprint)
+
 
 if __name__ == '__main__':
     app.run()
